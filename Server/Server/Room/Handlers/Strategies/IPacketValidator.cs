641ff7835a38a63ae189b0817533d75559a22bea
HAGGUM <guatn12@gmail.com>
2025-11-27 21:49:12 +0900

    baseRoom의 코드 중복 내용을 제거하기 위한 1차 phase 수정 - 패킷 응답, 패킷 검증, 패킷 응답/처리 결과 추가(전략 패턴)

---
+++ Server/Server/Room/Handlers/Strategies/IPacketValidator.cs
@@ -0,0 +1,24 @@
+﻿using Google.Protobuf;
+using Microsoft.Extensions.Logging;
+using Server.Core.Session;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Room.Handlers.Strategies
+{
+	/// <summary>
+	/// 패킷 검증 전략 인터페이스
+	/// </summary>
+	public interface IPacketValidator<TRequest> where TRequest : IMessage
+	{
+		// 검증 우선 순위
+		int Priority { get; }
+
+		// 패킷 검증
+		Task<ValidationResult> ValidateAsync( GameSession session, TRequest packet, BaseRoom room, ILogger logger );
+	}
+}
